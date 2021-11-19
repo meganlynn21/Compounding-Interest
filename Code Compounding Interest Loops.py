3798e1cf57d12924105c9a225233c22bb289b5ae
megankeyes <meganlkeyes0203@gmail.com>
2021-11-18 20:44:40 -0500

    Adding files from Linux

--- Code Compounding Interest Loops.py
+++ Code Compounding Interest Loops.py
@@ -4,7 +4,7 @@
 fDeposit = 0
 while fDeposit <= 0:
   try:
-    fDeposit = float(input('What is the original deposit (positive value): '))
+    fDeposit = float(input("What is the original deposit (positive value): "))
     if fDeposit <= 0:
       print("Input must be a positive numerical value: ")
   except ValueError:
@@ -13,7 +13,7 @@ while fDeposit <= 0:
 fInterest = 0
 while fInterest <= 0:
   try:
-    fInterest = float(input('What is the Interest Rate (positive value): '))
+    fInterest = float(input("What is the Interest Rate (positive value): "))
     if fInterest <= 0:
       print("Input must be a positive numerical value: ")
   except ValueError:
@@ -22,7 +22,7 @@ while fInterest <= 0:
 iMonths = 0
 while iMonths <= 0:
   try:
-    iMonths = int(input('What is the number of months (positive value): '))
+    iMonths = int(input("What is the number of months (positive value): "))
     if iMonths <= 0:
       print("Input must be a positive numerical value: ")
   except ValueError:
@@ -31,7 +31,7 @@ while iMonths <= 0:
 fGoal = None
 while fGoal == None:
   try:
-    fGoal = float(input('What is the goal amount (Can enter 0 but not negative): '))
+    fGoal = float(input("What is the goal amount (Can enter 0 but not negative): "))
     if fGoal < 0:
       print("Input must be a positive numerical value: ")
       fGoal = None
@@ -43,21 +43,24 @@ fMonthlyInterest= (fInterest/100/12)
 
 #Output the number of months the user has supplied and get account balance
 i = 1
+
 fAccountBalance = fDeposit
+
 while i <= fAccountBalance:
   if fAccountBalance >= fGoal:
     break
   fInterestForMonth = (fAccountBalance * fMonthlyInterest)
   #Add the interest for the month to the deposit to get the new Account Balance
   fAccountBalance += fInterestForMonth 
-  print('Month: ',i, 'Account Balance is: $',format(fAccountBalance, ",.2f"))
+  print("Month: ",i, "Account Balance is: $",format(fAccountBalance, ",.2f"))
   i += 1
+  
 # Calculate how many months it will take of compounding to reach the goal amount
 GoalFormatted = "{:.2f}".format(fGoal)
 CompoundedSavingsAccountBalance = fMonthlyInterest + fAccountBalance
 while CompoundedSavingsAccountBalance < fGoal:
   iMonths += 1 
-print('It will take: ',i-1, 'months to reach the goal of $', GoalFormatted)
+print("It will take: ",i-1, 'months to reach the goal of $", GoalFormatted)
   
 
    
@@ -71,4 +74,5 @@ print('It will take: ',i-1, 'months to reach the goal of $', GoalFormatted)
 
 
   
+
 
