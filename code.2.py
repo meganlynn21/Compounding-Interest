fc58fecdc186b96dbd88eb44b30f5aecae4dd3b4
keyesm <keyesm@student.elms.edu>
2021-12-09 01:39:55 -0500

    Fixed for loop

--- code.2.py
+++ code.2.py
@@ -1,4 +1,5 @@
 #Code Compounding Interest Loops Assignment
+#Megan Keyes
 
 #Keep asking until the user puts in a positive numeric value using loops
 fDeposit = 0
@@ -28,13 +29,13 @@ while iMonths <= 0:
   except ValueError:
     print("Input must be a numeric value")
 
-fGoal = -1
-while fGoal < 0:
+fGoal = None
+while fGoal == None:
   try:
     fGoal = float(input('What is the goal amount (Can enter 0 but not negative): '))
     if fGoal < 0:
       print("Input must be a positive numerical value: ")
-      fGoal = -1
+      fGoal = None
   except ValueError:
     print("Input must be a numeric value")
 
@@ -42,29 +43,35 @@ while fGoal < 0:
 fMonthlyInterest= (fInterest/100/12)
 
 #Output the number of months the user has supplied and get account balance
-fAccountBalance = fDeposit
 
+i = 1
+fAccountBalance = fDeposit
+while i <= 12:
+  if fAccountBalance >= fGoal:
+    break
+  fInterestForMonth = (fAccountBalance * fMonthlyInterest)
+  #Add the interest for the month to the deposit to get the new Account Balance
+  fAccountBalance += fInterestForMonth 
+  print('Month: ',i, 'Account Balance is: $',format(fAccountBalance, ",.2f"))
+  i += 1
 # Calculate how many months it will take of compounding to reach the goal amount
-for iMonth in range(0, iMonths):
-  fAccountBalance += fAccountBalance * fMonthlyInterest
-  print('Month: ',(iMonth + 1), 'Account Balance is: $',format(fAccountBalance, ",.2f"))
-
 GoalFormatted = "{:.2f}".format(fGoal)
+CompoundedSavingsAccountBalance = fMonthlyInterest + fAccountBalance
+while CompoundedSavingsAccountBalance < fGoal:
+  iMonths += 1 
+print('It will take: ',i-1, 'months to reach the goal of $', GoalFormatted)
   
-iMonth = 0
-fAccountBalance = fDeposit
-while fAccountBalance < fGoal:
-  fAccountBalance += fAccountBalance * fMonthlyInterest
-  iMonth += 1
-print('It will take: ',iMonths, 'months to reach the goal of $', GoalFormatted)  
+
+   
 
 
 
 
-
-
-  
 
 
   
 
+
+  
+
+##
